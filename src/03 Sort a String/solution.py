aad18bde34ab09d44cc0b3dc565c97a3eff18113
Sebastian Castro-Alvarez <secastroal@gmail.com>
2025-11-26 15:48:16 +0000

    Add comments to solution of sorting words.

--- src/03 Sort a String/solution.py
+++ src/03 Sort a String/solution.py
@@ -1,11 +1,14 @@
+# Create sorting function
 def sort_words(sentence):
+  # Split sentence in words
   sentence = [word for word in sentence.split(" ")]
-
+  # Sort words
   sentence = sorted(sentence, key = str.lower)
-
+  # Put words togueter
   sentence = ' '.join(sentence)
-  
+  # Return sorted sentence
   return sentence
 
+# Test sorting function
 print(sort_words('banana ORANGE apple'))
 print(sort_words('banana ORANGE apple cherry Beef grapes APRICOT'))
