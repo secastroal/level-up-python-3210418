83f03354dd6bdeb297b0c091e09d886b2ae0608b
Sebastian Castro-Alvarez <secastroal@gmail.com>
2025-11-26 15:41:19 +0000

    Add comments to solution palindrome.

--- src/02 Identify a Palindrome/solution.py
+++ src/02 Identify a Palindrome/solution.py
@@ -1,15 +1,17 @@
-
+# Define function to test if a character is a palindrome
 def is_palindrome(sentence):
-  
+  # turn to lower case and remove spaces and punctuation
   sentence = sentence.lower().replace(' ', '').replace('\'', '').replace('.', '')
-  
+  # Split by character
   tmp = [char for char in sentence]
+  # Reverse order
   tmp.reverse()
-
+  # Paste all character togueter 
   tmp = ''.join(tmp)
-  
+  # Return logical if sentence is equal to tmp
   return sentence == tmp
 
+# Testing phrases
 print(is_palindrome('Hello world'))
 print(is_palindrome('Thissiht'))
 print(is_palindrome('hello world'))
